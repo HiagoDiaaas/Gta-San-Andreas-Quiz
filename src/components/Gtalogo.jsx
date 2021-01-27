670a2682dfb7c06ecbe0677980d0fd4f26c091a2
Hiago <hiagosmd@hotmail.com>
2021-01-26 21:47:29 -0300

    first commit

---
+++ src/components/Gtalogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default () => {
+    return(
+        <div>
+        <img className="gtaLogo"src="https://upload.wikimedia.org/wikipedia/commons/b/b4/Grand_Theft_Auto_San_Andreas_logo.svg" alt="gta"/>
+        </div>
+    )
+}
